a9e212ea3c04a68a3bc6f2bb0836b552f987698d
ohyeong <oueong@naver.com>
2023-07-19 15:30:38 +0900

    update ch02

---
+++ src/ch_02/CharacterEx3.java
@@ -0,0 +1,15 @@
+package ch_02;
+
+public class CharacterEx3 {
+    public static void main(String[] args) {
+        int a = 65;
+        int b = -66;
+
+        char a2 = 65;
+        //char b2 = -66;   //오류 발생
+
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.println(a2);
+    }
+}
---
+++ src/ch_02/Variable1.java
@@ -0,0 +1,9 @@
+package ch_02;
+
+public class Variable1 {
+    public static void main(String[] args) {
+        int level;
+        level = 20;
+        System.out.println(level);
+    }
+}
